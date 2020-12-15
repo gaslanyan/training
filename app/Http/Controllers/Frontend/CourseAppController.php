88c2b7c2349475fae9df34e560eee76afb5e7fe6
Gayane-pc <sgayaneaslanyan@gmail.com>
2020-12-15 16:04:51 +0400

    Merge branch 'master' of https://github.com/gaslanyan/training
    # Conflicts:
    #	app/Http/Controllers/Backend/CoursesController.php
    #	database/migrations/2020_08_20_174605_2020_05_07_224021_add_foreign_keys_to_courses_table.php
    #	database/migrations/2020_08_20_174605_add_foreign_keys_to_courses_table.php
    #	database/migrations/2020_09_01_130610_add_foreign_keys_to_accounts_videos_table.php
    #	resources/sass/_custom.scss

--- app/Http/Controllers/Frontend/CourseAppController.php
+++ app/Http/Controllers/Frontend/CourseAppController.php
@@ -88,7 +88,7 @@ class CourseAppController extends Controller
             $data = [];
             $data['ClientID'] = '945431d0-ee02-4129-bacd-fc68eb0698ba';
             $data['Amount'] = 10;
-            $data['OrderID'] = 2357344;
+            $data['OrderID'] = 2357350;
             $data["BackURL"] = "https://www.shmz.am/lesson";
             $data['Username'] = '3d19541048';
             $data['Password'] = 'lazY2k';
