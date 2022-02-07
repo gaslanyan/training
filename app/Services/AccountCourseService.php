31539ef9df7b821cbe5a1b07ddad22fdcb740199
Gayane-pc <sgayaneaslanyan@gmail.com>
2022-02-07 19:47:32 +0400

    Payment notification

--- app/Services/AccountCourseService.php
+++ app/Services/AccountCourseService.php
@@ -131,6 +131,7 @@ class AccountCourseService
 
         } else {
             $paid = $this->getField($account_id, $course_id, 'paid');
+            dd($paid);
             $paid = ($paid) ? $paid->paid : 0;
         }
         $reading = $this->getField($account_id, $course_id, 'reading');
