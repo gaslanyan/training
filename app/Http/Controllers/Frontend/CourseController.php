2fabf34ce473eabe8b3aa8dc9d0b4495e1cd65e2
Gayane-pc <sgayaneaslanyan@gmail.com>
2022-02-15 23:51:20 +0400

    change book part

--- app/Http/Controllers/Frontend/CourseController.php
+++ app/Http/Controllers/Frontend/CourseController.php
@@ -75,7 +75,7 @@ class CourseController extends Controller
                 foreach ($books as $index => $book) {
                     $path = Config::get('constants.UPLOADS') . Config::get('constants.BOOKS') . $book;
                     $b = Book::select('id', 'title')->where('id', $book)->first();
-
+dd($this->service->getBook($book)->count);
                     $s3_books[$index] = $b;
                     $s3_books[$index]['count'] = $this->service->getBook($book)->count;
                     $s3_books[$index]['path'] = $path;
